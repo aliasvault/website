8f80563862533a7bb4dd35160bcdb3f0ea0946df
Leendert de Borst <ldeborst@xivisoft.com>
2026-07-09 21:35:26 +0200

    Update analytics

--- next.config.mjs
+++ next.config.mjs
@@ -55,11 +55,11 @@ const nextConfig = {
             key: 'Content-Security-Policy',
             value: [
               "default-src 'self'",
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://plausible.xivi.nl",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://analytics.aliasvault.com",
               "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
               "font-src 'self' https://fonts.gstatic.com",
               "img-src 'self' data: blob: https://img.shields.io https://www.gravatar.com",
-              "connect-src 'self' https://api.github.com https://plausible.xivi.nl",
+              "connect-src 'self' https://api.github.com https://analytics.aliasvault.com",
               "frame-src 'self' https://www.youtube.com https://youtube.com",
               "object-src 'none'",
               "base-uri 'self'",
--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -12,7 +12,10 @@ export default function RootLayout({
     <html suppressHydrationWarning>
       <head>
         <link rel="icon" href="/favicon.png" type="image/png" sizes="32x32" />
-        <Script src="https://plausible.xivi.nl/js/script.js" defer data-domain="aliasvault.com" />
+        <Script src="https://analytics.aliasvault.com/js/script.file-downloads.js" defer data-domain="aliasvault.com" />
+        <Script id="plausible-init">
+          {`window.plausible = window.plausible || function() { (window.plausible.q = window.plausible.q || []).push(arguments) }`}
+        </Script>
       </head>
       <body className={`bg-[#FCFCFC] dark:bg-black ${inter.className}`}>
         {children}
